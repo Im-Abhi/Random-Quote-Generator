a45fd32e8aab6f0b5d63e07e7a6833cc639f247b
Im-Abhi <abhishekgupta942001@gmail.com>
2021-09-08 12:56:15 +0530

    Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    return (
+            <div id="quote-box">
+              <div className="quote-text-box">
+                <i className="fa fa-quote-left"></i>
+                <span id="text"></span>
+              </div>
+              <div className="quote-author-box">- 
+                <span id="author"></span>
+              </div>
+              <div className="buttons-box">
+                <a 
+                  className="button"
+                  id="tweet-quote"
+                  title="Tweett this Quote!"
+                  target="_top"
+                  href="# "
+                >
+                  <i className="fa fa-twitter"></i> 
+                </a>
+                <button className="button" id="new-quote">New Quote</button>
+              </div>
+            </div>
+    );
 }
 
-export default App;
+export default App
