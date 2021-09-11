be893b3b218c685d0f4e4cba2be8e1c5f5d00246
Im-Abhi <abhishekgupta942001@gmail.com>
2021-09-11 12:03:13 +0530

    Add quotes API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App()
 {
+    const [ quote, setQuote ] = useState([]);
+    const [ randomQuote, setRandomQuote ] = useState([]);
+    useEffect(()=>{
+      async function fetchData(){
+        const response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+        const data = await response.json(); 
+        const quoteslist = data.quotes; 
+        setQuote(quoteslist);
+        setRandomQuote(quoteslist[Math.floor(Math.random()*quoteslist.length)]);
+      }
+      fetchData();
+    },[]);
+    function randomNumber(){
+      return Math.floor(Math.random()*quote.length);
+    }
+    const getNewQuote= () => {
+      const index = randomNumber();
+      setRandomQuote(quote[index])
+    }
     return (
             <div id="quote-box">
               <div className="quote-text-box">
                 <i className="fa fa-quote-left"></i>
-                <span id="text"></span>
+                <span id="text"
+                >{randomQuote['quote']}</span>
               </div>
               <div className="quote-author-box">- 
-                <span id="author"></span>
+                <span id="author">{randomQuote['author']}</span>
               </div>
               <div className="buttons-box">
                 <a 
@@ -22,7 +42,12 @@ function App()
                 >
                   <i className="fa fa-twitter"></i> 
                 </a>
-                <button className="button" id="new-quote">New Quote</button>
+                <button 
+                  className="button" 
+                  id="new-quote"
+                  onClick={getNewQuote}
+                >New Quote
+                </button>
               </div>
             </div>
     );
