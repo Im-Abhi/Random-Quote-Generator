559ede0b05bb34f261c0a148928c930fcc118cc7
Im-Abhi <abhishekgupta942001@gmail.com>
2021-09-12 17:15:32 +0530

    Add all the required styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App()
 {
     const [ quote, setQuote ] = useState([]);
     const [ randomQuote, setRandomQuote ] = useState([]);
+    const [ color , setColor ] = useState('#fe3');
     useEffect(()=>{
       async function fetchData(){
         const response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
@@ -12,18 +13,27 @@ function App()
         const quoteslist = data.quotes; 
         setQuote(quoteslist);
         setRandomQuote(quoteslist[Math.floor(Math.random()*quoteslist.length)]);
+        setColor(randomColor());
       }
       fetchData();
     },[]);
+    
     function randomNumber(){
       return Math.floor(Math.random()*quote.length);
+    }
+    function randomColor(){
+      const genColor = () => Math.floor(Math.random()*256);
+      return `rgb(${genColor()},${genColor()},${genColor()})`
     }
     const getNewQuote= () => {
       const index = randomNumber();
       setRandomQuote(quote[index])
+      setColor(randomColor);
     }
+    document.querySelector('body').style.backgroundColor = color
     return (
-            <div id="quote-box">
+            <div id="quote-box" 
+                  style={{color:color}}>
               <div className="quote-text-box">
                 <i className="fa fa-quote-left"></i>
                 <span id="text"
@@ -42,7 +52,7 @@ function App()
                 >
                   <i className="fa fa-twitter"></i> 
                 </a>
-                <button 
+                <button style={{backgroundColor:color}}
                   className="button" 
                   id="new-quote"
                   onClick={getNewQuote}
